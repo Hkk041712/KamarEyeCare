27a46269ccc378f86bfff090afb17bef984f8d51
Hkk041712 <158000393+Hkk041712@users.noreply.github.com>
2026-09-01 01:54:42 +0300

    manage patients updated - 5

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -3,11 +3,10 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from . import views
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('request-reset-otp/', views.request_password_reset_otp, name='request_reset_otp'),
-    path('confirm-reset/', views.confirm_password_reset, name='confirm_reset'),
-    
+    path('auth/login/', views.login_view, name='login'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/request-reset-otp/', views.request_password_reset_otp, name='request_reset_otp'),
+    path('auth/confirm-reset/', views.confirm_password_reset, name='confirm_reset'),
     path('products/', views.manage_products, name='manage_products'),
     path('products/<str:product_id>/', views.manage_products, name='delete_product'),
     path('sales/', views.manage_sales, name='manage_sales'),
